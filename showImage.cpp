47a9969d06159ae80bf13455db036028c753761e
littleshare <862420675@qq.com>
2019-03-06 20:01:54 +0800

    Add files via upload
    this is the first sample while is show a image.

---
+++ showImage.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "opencv2/opencv.hpp"
+
+int main(int argc, char** argv)
+{
+	IplImage *pic = NULL;
+	
+	pic = cvLoadImage("f:\\123.jpg");
+	if (NULL == pic)
+	{
+		printf("Load image fail!\n");
+		return -1;
+	}
+	cvNamedWindow("pic");
+	cvShowImage("pic", pic);
+	cvWaitKey(0);
+	
+	cvReleaseImage(&pic);
+	cvDestroyWindow("pic");
+	return 0; 
+} 
